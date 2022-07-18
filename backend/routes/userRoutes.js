25be86d1fdf2587a156944b694d0e64e30fdaf19
Mason Wagoner <mgwagoner@gmail.com>
2022-07-18 00:27:52 -0400

    added controller and updated routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const router = express.Router()
+const { registerUser, loginUser } = require('../controllers/userController')
+
 
 // New User
-router.post('/register', (req, res) => {
-  res.send('Register New User Route')
-})
+// router.post('/register', (req, res) => {
+//   res.send('Register New User Route')
+// })
+router.post('/register', registerUser)
 
 // Login
-router.post('/login', (req, res) => {
-  res.send('Login Route')
-})
+// router.post('/login', (req, res) => {
+//   res.send('Login Route')
+// })
+router.post('/login', loginUser)
+
 
 module.exports = router
