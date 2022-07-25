255cfdb3a7d747ca76d64a79b7747127e64a3037
Mason Wagoner <mgwagoner@gmail.com>
2022-07-25 19:57:00 -0400

    heroku troubleshoot 2

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 
