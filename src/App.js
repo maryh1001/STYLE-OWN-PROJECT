63ebc8209db3d9b1436df78408741116c39573e6
Priyanka Shah <priyanka2003shah@gmail.com>
2022-07-25 04:37:51 -0400

    using bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
-import HomeFeed from './components/HomeFeed';
 import Navigation from './components/Navigation';
-import ProfilePage from './components/ProfilePage';
-import Error404 from './components/Error404';
+import ProfilePage from './pages/ProfilePage';
+
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter> 
     <Navigation />
-    <ProfilePage /> 
+    <Routes>
+      {/* <Route exact path="/" component={HomeFeed}/> */}
+      <Route exact path="/profile" component={ProfilePage}/>
+      {/* <Route path="/" component={Error404} /> */}
+    </Routes>
     </BrowserRouter>
-    // <BrowserRouter> 
-    // <Navigation />
-    // <Routes>
-    //   <Route exact path="/" component={HomeFeed}/>
-    //   <Route exact path="/profile" component={ProfilePage}/>
-    //   <Route path="/" component={Error404} />
-    // </Routes>
-    // </BrowserRouter>
   );
 }
 
