ae9244d726cfa8b82f46ee961bd0d4d9682d0717
Mason Wagoner <mgwagoner@gmail.com>
2022-07-25 21:13:52 -0400

    uri issue

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 
 // Connect to database
