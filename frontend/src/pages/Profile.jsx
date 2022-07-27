f9e99de8da1a5d2246dbc24c1a84ccc84651e40e
Mason Wagoner <mgwagoner@gmail.com>
2022-07-27 17:44:49 -0400

    mock profile updated

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,59 +1,64 @@
 import {
-  FaUser,
-  FaAddressCard,
-  FaBookOpen,
   FaGithubSquare,
-  FaChess,
+  FaCouch,
 } from 'react-icons/fa';
 
 function Profile() {
   return (
     <>
       <section className='heading'>
-          <h1>User Profile</h1>
+          
+          <h3>User Profile</h3>
           <hr />
+          <p>About Me</p>
+          <br />
           <h6>Software Development Student</h6>
           <h6>NC State Software Development BootCamp</h6>
         
         <div>
           <br />
+          
           <h3>Hello User!</h3>
           <hr />
           <p>What would you like to work on?</p>
-
-          <h2>Project 1:</h2>
-          <p>
-            <a href='https://digitalskills.instructure.com/courses/5375'>
-              MySQL :
-            </a>
-          </p>
-          <p>To do: Updating the Routes</p>
-
-          <p>Project 2:</p>
-          <p>
-            <a href='https://digitalskills.instructure.com/courses/4915'>
-              React-Music-Library :
-            </a>
-          </p>
-          <p>description</p>
-
-          <p>Project 3:</p>
-          <p>
-            <a href='https://digitalskills.instructure.com/courses/5792'>
-              MileStone 2 :{' '}
-            </a>
-          </p>
-          <p>description</p>
-
-          <p>Project 4:</p>
-          <p>
-            <a href='https://digitalskills.instructure.com/courses/3811'>
-              CSS :
-            </a>
-          </p>
-          <p>description</p>
+          <br />
+          <a
+            href='https://github.com/MGWr6/Milestone-Project-1'
+            className='btn btn-reverse btn-block'
+          >
+            <FaGithubSquare /> Number Guesser
+          </a>
+          <a
+            href='https://github.com/MGWr6/react-a-mole'
+            className='btn btn-reverse btn-block'
+          >
+            <FaGithubSquare /> React-a-Mole
+          </a>
+          <a
+            href='https://github.com/MGWr6/Coder_Connect'
+            className='btn btn-reverse btn-block'
+          >
+            <FaGithubSquare /> CoderConnect
+          </a><a
+            href='https://github.com/MGWr6/Milestone-Project-1'
+            className='btn btn-reverse btn-block'
+          >
+            <FaGithubSquare /> Number Guesser
+          </a>
+          <a
+            href='https://github.com/MGWr6/react-a-mole'
+            className='btn btn-reverse btn-block'
+          >
+            <FaGithubSquare /> React-a-Mole
+          </a>
+          <a
+            href='https://github.com/MGWr6/Coder_Connect'
+            className='btn btn-reverse btn-block'
+          >
+            <FaGithubSquare /> CoderConnect
+          </a>
           </div>
-
+        <br />
         <div>
           <h3>Discover Projects!</h3>
           <hr />
@@ -76,21 +81,33 @@ function Profile() {
           >
             <FaGithubSquare /> CoderConnect
           </a>
+          <br />
           </div>
         
-        <p>Study Lounge:</p>
-          <p>
-            <a href='https://learnjs.slack.com/ssb/redirect'>Frontend</a>
-          </p>
-          <p></p>
-          <p>
-            <a href='https://discord.com/invite/yvtRAAaP'>
-              CodeSupport community
-            </a>
-          </p>
-          <p>
-            <a href='https://discord.com/invite/python'>Python</a>
-          </p>
+          <div>
+          <h3>Study Lounge</h3>
+          <hr />
+          <br />
+          <a
+            href='https://discordapp.com/invite/code'
+            className='btn btn-reverse btn-block'
+          >
+            <FaCouch /> DevCord
+          </a>
+          <a
+            href='https://discordsl.com/server/4837/lazy-developers'
+            className='btn btn-reverse btn-block'
+          >
+            <FaCouch /> Lazy Developer
+          </a>
+          <a
+            href='https://discord.me/coding'
+            className='btn btn-reverse btn-block'
+          >
+            <FaCouch /> Programmer's Hangout
+          </a>
+          </div>
+          <br />
         
       </section>
     </>
